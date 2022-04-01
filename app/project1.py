2226900afe315941b5150d8252b010d417284f0c
Sour_ce <gabrielhdezrodriguez@gmail.com>
2022-04-01 11:13:04 -0400

    Update project1.py

--- app/project1.py
+++ app/project1.py
@@ -1,5 +1,4 @@
 from queue import Queue
-import random
 import app.core.plugins as plug
 import app.core.simulation as sim
 import app.core.app as application
@@ -15,7 +14,11 @@ def is_ported(element:sim.SimElement):
         return False
   
 def resolve_element(name:str,sim_context:sim.SimContext)->sim.SimElement:
-    return next((e for e in sim_context.elements if e.name==name),None)
+    port=resolve_port(name,sim_context)
+    if port is None:
+        return next((e for e in sim_context.elements if e.name==name),None)
+    else:
+        return port.element
 
 class Cable():
     '''
@@ -89,49 +92,148 @@ class ConnectCMD(script.CommandDef):
         if (port1 is not None and port2 is not None):
             Cable(port1,port2)
         
+        
+def search_root(p:Port)->sim.SimElement:
+    if (p.element.get_element_type_name()=='pc'):
+        if (p.element.transmitting==True):
+            return p.element
+        else:
+            return None
+    else:
+        i_port=next(port for port in p.element.ports if port.id==p.element.input_port)
+        if i_port is p:
+            return None
+        else:
+           c:Cable=i_port.cable
+           next_p=next((p for p in c.ports if p is not i_port))
+           return search_root(next_p)
+
 class DisconnectCMD(script.CommandDef):
     def run(self, sim_context:sim.SimContext, port, *params):
         port=resolve_port(port,sim_context)
         if(port is not None):
             c=port.cable
-            c.ports[0].cable=None
-            c.ports[1].cable=None
-            del(c)
+            if (c is not None):
+                c.ports[0].cable=None
+                c.ports[1].cable=None
+                #Must cut the transmition
+                if(c.transmitting):
+                    leftPort:Port=c.ports[0]
+                    rightPort:Port=c.ports[1]
+                    
+                    el=search_root(leftPort)
+                    er=search_root(rightPort)
+                    
+                    q=Queue()
+                    q.put(el if er is None else er)
+                    while not q.empty():
+                        end_transmition(q,q.get())
+                del(c)
 
 def read_data(bfs_q:Queue,e:sim.SimElement):
-    if isinstance(e,PC):
-        e.output(f"{e.context.time} {e.name} recieve {e.port.cable.data}")
-    if isinstance(e,Hub):
+    if e.get_element_type_name()=='pc':
+        e.output(f"{e.context.time} {e.port} recieve {e.port.cable.data}")
+    if e.get_element_type_name()=='hub':
         i_port,index=next((port,port.id) for port in e.ports if port.cable.transmitting)
         e.input_port=index
-        e.output(f"{e.context.time} {e.name} recieve {i_port.cable.data}")
+        e.output(f"{e.context.time} {i_port} recieve {i_port.cable.data}")
         bfs_q.put((1,e))
         
 def write_data(bfs_q:Queue,e:sim.SimElement):
-    #TODO: Imp
-    pass
+    if e.get_element_type_name()=='hub':
+        i_port=next(port for port in e.ports if port.id==e.input_port)
+        data=i_port.cable.data
+        out_ports=(port for port in e.ports if port.id!=e.input_port and port.cable is not None)
+        e.transmitting=True
+        for port in out_ports:
+            c:Cable=port.cable
+            out_port:Port=next((p for p in c.ports if p is not port))
+            c.transmitting=True
+            c.data=data
+            e.output(f"{e.context.time} {port} send {data}")
+            bfs_q.put((0,out_port.element))
+        bfs_q.put((1,e))
 
+def end_transmition(bfs_q:Queue,e:sim.SimElement):
+    if e.get_element_type_name()=='pc':
+        c:Cable=e.port.cable
+        if c is not None:
+            c.data='0'
+            c.transmitting=False
+    if e.get_element_type_name()=='hub':
+        i_port:Port=next(port for port in e.ports if port.id==e.input_port)
+        out_ports=(port for port in e.ports if port.id!=e.input_port and port.cable is not None)
+        if (i_port.cable is not None):
+            i_port.cable.data='0'
+            i_port.cable.transmitting=False
+        e.transmitting=False
+        for port in out_ports:
+            bfs_q.put(port.element)
+
+class EndSendingCMD(script.CommandDef):
+    def run(self, sim_context, host, *params):
+        host:PC=resolve_element(host,sim_context)
+        q=Queue()
+        if host is not None and host.port.cable is not None:
+            host.transmitting=False
+            out_port:Port=next((p for p in host.port.cable.ports if p is not host.port))
+            q.put(out_port.element)
+            while not q.empty():
+                e=q.get()
+                end_transmition(q,e)
+    
 class SendCMD(script.CommandDef):
     def run(self, sim_context:sim.SimContext, host, data, *params):
         host:PC=resolve_element(host,sim_context)
-        data_n=data[0]
-        data_r=data[1:]
+        data_n=data[0] #Current bit to send
+        data_r=None
+        #Rest of the bits
+        if (len(data)>1):
+            data_r=data[1:]
         q=Queue()
-        if host.port.cable.transmitting:
-            #TODO: Delay the sending
-            pass
-        host.port.cable.transmitting=True
-        host.port.cable.data=data_n
-        out_port:Port=next((p for p in host.port.cable.ports if p is not host.port))
-        q.put((0,out_port.element))
-        if host is not None:
+        if host is not None and host.port.cable is not None:
+            if host.port.cable.transmitting:
+                #Delay the current sending a time different for each host
+                sim_context.p_queue.add_early(script.SubCommand(
+                    sim_context.time+(
+                    hash(host.name)
+                    %
+                    50
+                    ),SendCMD(),host.name,data,*params))
+                return
+            #First step of the sending(BFS)
+            host.transmitting=True
+            host.port.cable.transmitting=True
+            host.port.cable.data=data_n
+            host.output(f"{host.context.time} {host.port} send {data_n}")
+            out_port:Port=next((p for p in host.port.cable.ports if p is not host.port))
+            #The queue starts with the first reciever of the signal in it
+            q.put((0,out_port.element))
             while not q.empty():
-                op,e,d=q.get()
+                op,e=q.get()
                 if (op==0):
+                    #Read the data that reach the element and puts in the queue the writing action if
+                    #it's a hub
                     read_data(q,e)
                 else:
+                    #Write data among the ports and puts in the queue the reading actions of each reciever
+                    #element
                     write_data(q,e)
-            #TODO: Schedule the rest of the sending and the connection end
+            #Schedule the rest of the sending and the connection end
+            if data_r is not None:
+                #Send the rest
+                sim_context.p_queue.add_early(script.SubCommand(
+                    sim_context.time+sim_context.app.config['signal_time'],
+                    SendCMD(),
+                    host.name,data_r,
+                    *params))
+            #Shutdown the transmitting
+            sim_context.p_queue.add_early(script.SubCommand(
+                sim_context.time+sim_context.app.config['signal_time'],
+                EndSendingCMD(),
+                host.name,
+                *params))
+            
                     
 
 class BasicInit(plug.PluginInit1):
@@ -151,3 +253,6 @@ class CommandsInit(plug.PluginInit1):
         app.commands['create']=CreateCMD()
         app.commands['connect']=ConnectCMD()
         app.commands['disconnect']=DisconnectCMD()
+        app.commands['send']=SendCMD()
+        
+        app.pv_commands['endsend']=EndSendingCMD()
