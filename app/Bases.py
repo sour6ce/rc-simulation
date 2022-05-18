75c991d4874bacf244f6394a326e9481a89d6c9e
Sour_ce <gabrielhdezrodriguez@gmail.com>
2022-05-18 14:33:42 -0400

    Refactorization for 3rd layer started with tests

---
+++ app/Bases.py
@@ -0,0 +1,69 @@
+from typing import List
+from typing_extensions import Self
+
+
+class SimData():
+    __match_args__ = ("data")
+    data = []
+
+    def __init__(self, data: str | List[bool] | Self | None = None):
+        match data:
+            case str():
+                data = data.replace(' ', '')
+                val: int = 0
+                try:
+                    val = int(data, 2)
+                except:
+                    try:
+                        val = int(data, 16)
+                    except:
+                        raise ValueError(
+                            "data argument should be a string of mostly 1's or 0's" +
+                            ", a list of booleans or another SimData")
+                data = bin(val)[2:]
+                self.data: List[bool] =\
+                    [True if c == '1' else False
+                        for c in data if c == '0' or c == '1']
+            case SimData():
+                self.data: List[bool] = data.data.copy()
+            case [*_data] if all(isinstance(i, bool) for i in _data):
+                self.data: List[bool] = data.copy()
+            case None:
+                pass
+            case _:
+                raise ValueError(
+                    "data argument should be a string of mostly 1's or 0's" +
+                    ", a list of booleans or another SimData")
+
+    def getData(self) -> List[bool]:
+        return self.data
+
+    def __eq__(self, __o: object) -> bool:
+        return all((i == j for i, j in zip(self.data, SimData(__o).data)))
+
+    def insert(self, index: int, value: bool) -> None:
+        self.data.insert(index, value)
+
+    def __str__(self) -> str:
+        return self.tobin()
+
+    def __repr__(self) -> str:
+        return repr(list(self.tobin()))
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def tobin(self, complete_bytes=False) -> str:
+        d_len = len(self)
+        r = ''.join('1' if v else '0' for v in self.data)
+        if (d_len % 8 != 0 and complete_bytes):
+            return ''.join([((((d_len//8)+1)*8)-d_len)*'0', r])
+        return r
+
+    def tohex(self, complete_bytes=False) -> str:
+        d_len = len(self)
+        b = int(self.tobin(True), 2)
+        r = str.upper(hex(b)[2:])
+        if (len(r) % 2 != 0 and complete_bytes):
+            return ''.join(['0', r])
+        return r
---
+++ tests/test_Bases.py
@@ -0,0 +1,45 @@
+import unittest
+from app.Bases import *
+
+
+class TestData(unittest.TestCase):
+    def test_repr(self):
+        a = SimData()
+        a.data = [True, True, False, True]
+        self.assertEqual(repr(a), repr(['1', '1', '0', '1']))
+
+    def test_str(self):
+        a = SimData()
+        a.data = [True, True, False, True]
+        self.assertEqual(str(a), '1101')
+
+    def test_tobin_cb(self):
+        a = SimData()
+        a.data = [True, True, True, False, True]
+        self.assertEqual(a.tobin(True), '00011101')
+
+    def test_hexform_coff(self):
+        a = SimData()
+        a.data = [True, True, True, False, True]
+        self.assertEqual(a.tohex(), '1D')
+
+    def test_hexform_con(self):
+        a = SimData()
+        a.data = [True, True, False, True]
+        self.assertEqual(a.tohex(True), '0D')
+
+    def test_ctor_vs_hex(self):
+        a = SimData('1111 0111 1001 1010 0000')
+        self.assertEqual(a.tohex(True), '0F79A0')
+
+    def test_ctor_bl(self):
+        a = SimData([True, False, False, False, True])
+        self.assertEqual(a.tobin(True), '00010001')
+
+    def test_ctor_copy(self):
+        a = SimData('FF 00')
+        b = SimData(a)
+
+        b.insert(0, True)
+
+        self.assertNotEqual(a.tohex(True), b.tohex(True))
