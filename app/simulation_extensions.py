0f5c52dfc615eb481d19d937a9a8ca9170b5497f
Sour_ce <gabrielhdezrodriguez@gmail.com>
2022-05-25 18:04:23 -0400

    Simulation functions and whole timer test

--- app/simulation_extensions.py
+++ app/simulation_extensions.py
@@ -1,13 +1,60 @@
-from ast import Call
 import logging
-from typing import Callable, List
-from app.core.main import Application
-from app.bitwork import uint
-import app.core.main as main
-import app.core.main as sim
-import app.core.main as plug
+from typing import Iterable, List, Type
+from app.core.main import Application, CommandDef, MissingCommandDefinition, PluginInit1, SimContext, SimElement, SubCommand
 
 LOAD_ORDER = -1
+
+
+def get_commanddef_byname(name: str) -> CommandDef | None:
+    cmd: CommandDef | None = Application.instance.commands[name] if name in \
+        Application.instance.commands.keys() else None
+    if cmd is None:
+        cmd = Application.instance.pv_commands[name] if name in \
+            Application.instance.pv_commands.keys() else None
+
+    return cmd
+
+
+def get_element_type_byname(name: str) -> SimElement | None:
+    return Application.instance.elements[name] if \
+        name in Application.instance.elements.keys() else None
+
+
+def get_elements_byname(name: str) -> Iterable[SimElement]:
+    return (e for e in Application.instance.simulation.elements if e.name == name)
+
+
+def get_element_byname(name: str) -> SimElement | None:
+    return next(get_elements_byname(name), None)
+
+
+def get_elements_bytype(t: Type) -> Iterable[SimElement]:
+    return (e for e in Application.instance.simulation.elements if isinstance(e, t))
+
+
+def get_element_bytype(type: Type) -> SimElement | None:
+    return next(get_elements_bytype(type), None)
+
+
+def create_element(type_name: str, name: str, *params, **kwargs) -> SimElement:
+    sim_context = Application.instance.simulation
+    sim_context.elements.append(
+        get_element_type_byname(str(type_name))(str(name), sim_context, *(str(v) for v in params), **kwargs))
+    return sim_context.elements[-1]
+
+
+def schedule_command(time: int, cmd: str, *params, early=True) -> SubCommand:
+    time = Application.instance.simulation.time+int(time)
+    cmd = str(cmd)
+    sub_cmd = SubCommand(time, get_commanddef_byname(cmd), *params)
+    if sub_cmd.cmddef is None:
+        raise MissingCommandDefinition("${cmd} command definition not found")
+    if early:
+        Application.instance.simulation.p_queue.add_early(sub_cmd)
+    else:
+        Application.instance.simulation.p_queue.add_late(sub_cmd)
+
+    return sub_cmd
 
 
 def schedule_forced_update(time: int, early=True) -> None:
@@ -16,28 +63,24 @@ def schedule_forced_update(time: int, early=True) -> None:
             cmd.time == time), None) is not None:
         return
     if early:
-        Application.instance.simulation.p_queue.add_early(main.SubCommand(
+        Application.instance.simulation.p_queue.add_early(SubCommand(
             time, Application.instance.pv_commands['blank']))
     else:
-        Application.instance.simulation.p_queue.add_late(main.SubCommand(
+        Application.instance.simulation.p_queue.add_late(SubCommand(
             time, Application.instance.pv_commands['blank']))
 
 
 def execute_command(name: str, *params: List[str]) -> None:
-    c: main.CommandDef | None = Application.instance.commands[name] if name in \
-        Application.instance.commands.keys() else None
-    if c is None:
-        c = Application.instance.pv_commands[name] if name in \
-            Application.instance.pv_commands.keys() else None
+    c = get_commanddef_byname(name)
 
     if (c is None):
-        raise main.MissingCommandDefinition(
+        raise MissingCommandDefinition(
             f"Impossible to execute {name}, "+"command definition not found")
     else:
         c.run(Application.instance.simulation, *params)
 
 
-class WrapperCMD(main.CommandDef):
+class WrapperCMD(CommandDef):
     def run(self, sim_context, keyword: str, originalcmd: str, *params: List[str]):
         try:
             if callable(Application.instance.wrap_handler):
@@ -47,8 +90,8 @@ class WrapperCMD(main.CommandDef):
             pass
 
 
-class PrintCMD(main.CommandDef):
-    def run(self, sim_context: sim.SimContext, msg: str = 'DEBUG_PRINT',
+class PrintCMD(CommandDef):
+    def run(self, sim_context: SimContext, msg: str = 'DEBUG_PRINT',
             level: str = '10',  *params):
         logging.log(int(level), msg)
 
@@ -57,7 +100,7 @@ def basic_wrap_handler(keyword: str, originalcmd: str, *params: List[str]):
     execute_command(originalcmd, *params)
 
 
-class DebugInit(plug.PluginInit1):
+class DebugInit(PluginInit1):
     def run(self, app: Application, *args, **kwargs):
         app.commands['print'] = PrintCMD()
         app.commands['$'] = WrapperCMD()
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,8 +1,10 @@
 import logging
+from sqlite3 import Time
 from typing import List
 from tests.sim import SimulationTest
 import app.simulation_extensions as sime
 from app.core.main import Application, CommandDef
+from app.timer import Timer
 
 
 class TestTimer(SimulationTest):
@@ -21,6 +23,24 @@ class TestTimer(SimulationTest):
         blank_cmd.run = lambda *params: logging.debug(
             f"Executed update at "+str(Application.instance.simulation.time))
 
+        timer: Timer = sime.create_element('timer', 'alt', 50)
+        timer.add_time_passed_callback(
+            lambda: logging.info("Callback on timer alt executed"))
+
         while self.advance():
             pass
+
+        items = [e.name for e in sime.get_elements_bytype(Timer)]
+        items_by_name = [
+            sime.get_element_byname("alt").name,
+            sime.get_element_byname("t1").name,
+            sime.get_element_byname("t2").name,
+            sime.get_element_byname("t3").name
+        ]
+        self.assertListEqual(items, items_by_name)
+        self.assertListEqual(items, ['alt', 't1', 't2', 't3'])
         
+        sime.schedule_command(20,"$",'debug','print','all_well')
+        
+        while self.advance():
+            pass
