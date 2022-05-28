a8b0d329888a9b0ed5372381ff5ebacd17ae296d
Sour_ce <gabrielhdezrodriguez@gmail.com>
2022-05-28 16:14:26 -0400

    Data Timeout

--- app/ported.py
+++ app/ported.py
@@ -1,7 +1,9 @@
+from app.timer import Timer
 from typing import Iterable, List
 from app.core.main import Application, SimContext, SimElement
 from app.port import Port
-from app.extensions import get_element_byname
+from app.extensions import create_element, get_element_byname
+from app.ported import delete_element
 
 
 def isported(element: SimElement) -> bool:
@@ -42,6 +44,29 @@ class PortedElement(SimElement):
         self.__ports = [Port(self.name+'_'+str(i+1))
                         for i in range(int(default_ports))]
 
+        def get_timeout_callback(port: Port):
+            def wrap():
+                port.get_data_eater().clear()
+            return wrap
+
+        def data_timeout_timer(port: Port):
+            def wrap():
+                old_timer: Timer = None
+                try:
+                    old_timer = get_element_byname(f"{port}_timeout_timer")
+                    old_timer.curent_time
+                except:
+                    pass
+                if (old_timer is not None):
+                    delete_element(f"{port}_timeout_timer")
+                timer: Timer = create_element(
+                    'timer',
+                    f"{port}_timeout_timer",
+                    Application.config['data_input_timeout']
+                )
+                timer.add_time_passed_callback(get_timeout_callback(port))
+            return wrap
+
         for port in self.__ports:
             port.add_data_send_started_callback(lambda x: self.output(
                 f"{Application.instance.simulation.time} {port} send {'1' if x else '0'}"
@@ -49,21 +74,11 @@ class PortedElement(SimElement):
             port.add_data_recieve_started_callback(lambda x: self.output(
                 f"{Application.instance.simulation.time} {self} recieve {'1' if x else '0'}"
             ))
+            if int(Application.config['data_input_timeout']) >= 0:
+                port.add_data_send_finished_callback(data_timeout_timer(port))
 
     def get_ports(self) -> List[Port]:
         return self.__ports
-
-    def add_port(self, id: str) -> Port:
-        p = Port(id)
-        self.__ports.append(p)
-
-    def remove_port(self, id: str) -> bool:
-        p = next((p for p in self.__ports if p.getid() == id), None)
-        if p == None:
-            return False
-        else:
-            self.__ports.remove(p)
-            return True
 
     def has_port(self, port: Port) -> bool:
         return next((p for p in self.__ports if p == port), None) != None
